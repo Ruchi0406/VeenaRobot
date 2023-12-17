0cecc81efa396a0152af432dbc3d368d41e36d8d
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2023-12-18 00:19:03 +0530

    Update memify.py

--- MukeshRobot/modules/memify.py
+++ MukeshRobot/modules/memify.py
@@ -1,48 +1,139 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from PIL import Image, ImageDraw, ImageFont
+import textwrap
 import os
+from PIL import Image, ImageFont, ImageDraw
+from MukeshRobot.events import register
+from MukeshRobot import telethn as bot
 
-# Function to handle the /mmf command
-def memify(update: Update, context: CallbackContext) -> None:
-    # Check if the message contains a photo or sticker
-    if update.message.photo or update.message.sticker:
-        # Determine the file ID based on the content type
-        if update.message.photo:
-            file_id = update.message.photo[-1].file_id
-        else:  # If it's a sticker
-            file_id = update.message.sticker.file_id
 
-        # Download the file
-        file = context.bot.get_file(file_id)
-        file.download('input_file')
+@register(pattern="^/mmf ?(.*)")
+async def handler(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.reply("`Provide Some Text To Draw! And Reply To Image/Stickers EXAMPLE: /mmf text`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await event.reply("```Reply to a image/sticker.```")
+        return
+    file = await bot.download_media(reply_message)
+    msg = await event.reply("```Memifying this image! (」ﾟﾛﾟ)｣ ```")
+    text = str(event.pattern_match.group(1)).strip()
+    if len(text) < 1:
+        return await msg.edit("You might want to try `/mmf text`")
+    meme = await drawText(file, text)
+    await bot.send_file(event.chat_id, file=meme, force_document=False)
+    await msg.delete()
+    os.remove(meme)
 
-        # Open the file using PIL
-        image = Image.open('input_file')
 
-        # Add text to the image
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.load_default()
-        draw.text((10, 10), "Memified by Veena Music", font=font, fill="white")
+# Taken from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/memify.py#L64
+# Maybe replyed to suit the needs of this module
 
-        # Save the memified image
-        image.save('output_meme.jpg')
 
-        # Send the memified image back to the user
-        context.bot.send_photo(update.message.chat_id, photo=open('output_meme.jpg', 'rb'))
-
-        # Clean up temporary files
-        os.remove('input_file')
-        os.remove('output_meme.jpg')
+async def drawText(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    shadowcolor = "black"
+    i_width, i_height = img.size
+    if os.name == "nt":
+        fnt = "ariel.ttf"
     else:
-        update.message.reply_text("Please send a photo or sticker to memify!")
+        fnt = "./EmikoRobot/resources/default.ttf"
+    m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
 
-# Set up the bot with the token from environment variable
-updater = Updater(os.getenv("TOKEN"))
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
 
-# Set up command handlers
-updater.dispatcher.add_handler(CommandHandler("mmf", memify))
-
-# Start the bot
-updater.start_polling()
-updater.idle()
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    image_name = "memify.webp"
+    webp_file = os.path.join(image_name)
+    img.save(webp_file, "webp")
+    return webp_file
